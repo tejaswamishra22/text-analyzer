676b31484fc54fbeebcc0fb11154fe6f51230431
Tejaswa Mishra <tejaswamishra22@gmail.com>
2026-03-20 17:23:42 +0530

    Added model file

--- src/main/java/com/analyzer/model/AnalysisResult.java
+++ src/main/java/com/analyzer/model/AnalysisResult.java
@@ -1,3 +1,15 @@
-public class AnalysisResult {
-    
-}
+package com.analyzer.model;
+
+import java.util.List;
+import java.util.Map;
+
+public record AnalysisResult(
+    int wordCount,
+    int sentenceCount,
+    int paragraphCount,
+    double readingTimeMinutes,
+    List<String> topKeywords,
+    String sentiment,
+    double avgWordsPerSentence,
+    Map<String, Integer> keywordFrequency
+) {}
